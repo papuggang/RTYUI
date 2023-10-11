f723575bdf81c4001e9f404b286f2f07f01b2b8a
jy7hjy7h6fy7 <@>
2023-10-11 14:29:59 +0300

    ghjkg

---
+++ ConsoleApp1/ConsoleApp1/Sum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Sum
+    {
+        public int sum()
+
+        {
+            int a = 4;
+            int b = 5;
+            int Sum = a + b;
+            return Sum;
+        }
+        
+    }
+}
